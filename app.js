14848cc1b1f92c550d65d2ac18ba1f63d97c9afe
Karan Singh <ZucM@users.noreply.github.com>
2021-01-23 14:01:57 +0530

    restarting

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
-
-
-
-
-function clickEventHandler() {
-    outputDiv.innerText = "translated: " + txtInput.value; 
-}
-
-btnTranslate.addEventListener("click",clickEventHandler)
+var btnTranslate = document.querySelector('#btn-translate')
+console.log(btnTranslate)
